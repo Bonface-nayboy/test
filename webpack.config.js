cc6610862029c9687a2d30319a78c38c5a3fcfa6
Bonface-nayboy <bonnieway615@gmail.com>
2024-11-03 09:45:30 +0300

    Committing changes before upgrading Expo

--- webpack.config.js
+++ webpack.config.js
@@ -5,19 +5,15 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 module.exports = async function (env, argv) {
   const config = await createExpoWebpackConfigAsync(env, argv);
 
-  // Your modifications here...
-  
   // Set the entry point to your App.js file
   config.entry = './App.js';
 
   // Set output configuration
-  
   config.output = {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].[contenthash].js', // Ensure contenthash usage
     clean: true,
   };
-  
 
   // Include the fallback for Node.js modules
   config.resolve.fallback = {
@@ -43,7 +39,7 @@ module.exports = async function (env, argv) {
   config.module.rules.push({
     test: /\.css$/,
     use: [
-      'style-loader', 
+      'style-loader',
       {
         loader: 'css-loader',
         options: {
@@ -53,6 +49,27 @@ module.exports = async function (env, argv) {
       },
     ],
   });
+
+  // Add rules for handling fonts and images
+  config.module.rules.push({
+    test: /\.(woff|woff2|eot|ttf|otf|png|jpe?g|gif|svg)$/,
+    use: [
+      {
+        loader: 'file-loader',
+        options: {
+          name: '[path][name].[ext]',
+          outputPath: 'assets/',
+        },
+      },
+    ],
+  });
+
+  // Set performance hints
+  config.performance = {
+    hints: 'warning',
+    maxAssetSize: 1024 * 1024, // 1 MB
+    maxEntrypointSize: 1024 * 1024, // 1 MB
+  };
 
   // Add the CleanWebpackPlugin
   config.plugins.push(new CleanWebpackPlugin());
